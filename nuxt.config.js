34cc4d0c2623634c200826df19fb0d01d25a038f
David <david@remote-tech.co.uk>
2020-06-05 15:55:08 +0100

    OG

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Official Website' },
-      { property: 'og-image', content: '/thumbnail.png' }
+      { hid: 'og-image', property: 'og-image', content: '/thumbnail.png' },
+      { hid: 'og:description', property: 'og:description', content: 'Official Website' },
+      { hid: 'og:title', property: 'og:title', content: 'MEDEEA' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
