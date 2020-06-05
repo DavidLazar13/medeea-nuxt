514aab4cae3f2e783a116cb4278c8f3ef8c2c805
David <david@remote-tech.co.uk>
2020-06-05 14:33:15 +0100

    thumbnail

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Official Website' }
+      { hid: 'description', name: 'description', content: 'Official Website' },
+      { hid: 'og-image', property: 'og-image', content: '/thumbnail.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
